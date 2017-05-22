3f6646d6cc9fe0e508fda48ea4626c4acf442420
Guodong Xu <guodong.xu@linaro.org>
2017-05-23 07:09:28 +0800

    fix: bluetooth: hci_ll: set enable_gpio direction OUT at probe
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- drivers/bluetooth/hci_ll.c
+++ drivers/bluetooth/hci_ll.c
@@ -708,6 +708,7 @@ static int hci_ti_probe(struct serdev_device *serdev)
 	struct hci_uart *hu;
 	struct ll_device *lldev;
 	u32 max_speed = 3000000;
+	int err;
 
 	lldev = devm_kzalloc(&serdev->dev, sizeof(struct ll_device), GFP_KERNEL);
 	if (!lldev)
@@ -720,6 +721,12 @@ static int hci_ti_probe(struct serdev_device *serdev)
 	lldev->enable_gpio = devm_gpiod_get_optional(&serdev->dev, "enable", GPIOD_OUT_LOW);
 	if (IS_ERR(lldev->enable_gpio))
 		return PTR_ERR(lldev->enable_gpio);
+
+	err = gpiod_direction_output(lldev->enable_gpio, 0);
+	if (unlikely(err)) {
+		bt_dev_err(hu->hdev, "unable to configure enable-gpio");
+		return err;
+	}
 
 	of_property_read_u32(serdev->dev.of_node, "max-speed", &max_speed);
 	hci_uart_set_speeds(hu, 115200, max_speed);
